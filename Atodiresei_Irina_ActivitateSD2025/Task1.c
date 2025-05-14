c2dbd13cbbcf882f00c6d4001fcaa06f87d57b32
Irina Ato <irinaato77@gmail.com>
2025-05-14 23:25:01 +0300

    Task1

---
+++ Atodiresei_Irina_ActivitateSD2025/Task1.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    char* adresa;
+    int anConstructie;
+    float suprafata;
+} Imobiliar;
+
+Imobiliar citireImobiliar() {
+    Imobiliar i;
+    char buffer[100];
+
+    printf("ID: ");
+    scanf("%d", &i.id);
+    getchar();
+
+    printf("Adresa: ");
+    fgets(buffer, 100, stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
+    i.adresa = (char*)malloc(strlen(buffer) + 1);
+    strcpy(i.adresa, buffer);
+
+    printf("An constructie: ");
+    scanf("%d", &i.anConstructie);
+
+    printf("Suprafata: ");
+    scanf("%f", &i.suprafata);
+
+    return i;
+}
+
+void afisareImobiliar(Imobiliar i) {
+    printf("ID: %d\n", i.id);
+    printf("Adresa: %s\n", i.adresa);
+    printf("An constructie: %d\n", i.anConstructie);
+    printf("Suprafata: %.2f mp\n", i.suprafata);
+    printf("\n");
+}
+
+float mediaSuprafata(Imobiliar i) {
+    return i.suprafata;
+}
+
+void modificaAdresa(Imobiliar* i, const char* nouaAdresa) {
+    free(i->adresa);
+    i->adresa = (char*)malloc(strlen(nouaAdresa) + 1);
+    strcpy(i->adresa, nouaAdresa);
+}
+
+void dezalocare(Imobiliar* i) {
+    if (i->adresa) {
+        free(i->adresa);
+        i->adresa = NULL;
+    }
+}
+
+int main() {
+    Imobiliar i1 = citireImobiliar();
+    afisareImobiliar(i1);
+
+    printf("Suprafata medie: %.2f\n", mediaSuprafata(i1));
+
+    modificaAdresa(&i1, "Str. Noua nr. 100");
+    printf("Dupa modificarea adresei:\n");
+    afisareImobiliar(i1);
+
+    dezalocare(&i1);
+    return 0;
+}
--- Atodiresei_Irina_ActivitateSD2025/alta_forma_seminar1.c
+++ Atodiresei_Irina_ActivitateSD2025/alta_forma_seminar1.c
@@ -1,70 +1,70 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-struct Elev
-{
-	int idElev;
-	int nrMaterii;
-	float* note;
-	char* numeElev;
-	int nrOptionale;
-};
-
-struct Elev initializare(int idElev, int nrMaterii, float* note, char* numeElev, int nrOptionale)
-{
-	struct Elev e;
-	e.idElev = idElev;
-	e.nrMaterii = nrMaterii;
-	e.note = (float*)malloc(nrMaterii*sizeof(float));
-	for (int i = 0; i < nrMaterii; i++)
-	{
-		e.note[i] = note[i];
-	}
-	e.numeElev = (char*)malloc(strlen(numeElev) + 1);
-	strcpy_s(e.numeElev, strlen(numeElev) + 1, numeElev);
-	e.nrOptionale = nrOptionale;
-	return e;
-}
-
-
-
-void afisare(struct Elev e)
-{
-
-	printf("\n Id elev: %d\n Nr materii: %d\n Nume elev: %s\n Nr de optionale alese sunt: %d\n Notele sunt: \n ", e.idElev, e.nrMaterii, e.numeElev,e.nrOptionale);
-	for (int i = 0; i < e.nrMaterii; i++)
-	{
-		printf("\n%.2f\n", e.note[i]);
-	}
-
-}
-
-void modificare_nrOptionale(struct Elev* e, int nrNou)
-{
-	if (nrNou > 0 && nrNou<5)
-	{
-		e->nrOptionale = nrNou;
-	}
-}
-
-void dezalocare(struct Elev* e)
-{
-	free(e->note);
-	e->note = NULL;
-
-	free(e->numeElev);
-	e->numeElev = NULL;
-}
-
-
-int main()
-{
-	float note[] = { 10,9.7 };
-	struct Elev e1 = initializare(1, 2, note, "ionel",3);
-	afisare(e1);
-	modificare_nrOptionale(&e1,4);
-	afisare(e1);
-
-	dezalocare(&e1);
-
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//struct Elev
+//{
+//	int idElev;
+//	int nrMaterii;
+//	float* note;
+//	char* numeElev;
+//	int nrOptionale;
+//};
+//
+//struct Elev initializare(int idElev, int nrMaterii, float* note, char* numeElev, int nrOptionale)
+//{
+//	struct Elev e;
+//	e.idElev = idElev;
+//	e.nrMaterii = nrMaterii;
+//	e.note = (float*)malloc(nrMaterii*sizeof(float));
+//	for (int i = 0; i < nrMaterii; i++)
+//	{
+//		e.note[i] = note[i];
+//	}
+//	e.numeElev = (char*)malloc(strlen(numeElev) + 1);
+//	strcpy_s(e.numeElev, strlen(numeElev) + 1, numeElev);
+//	e.nrOptionale = nrOptionale;
+//	return e;
+//}
+//
+//
+//
+//void afisare(struct Elev e)
+//{
+//
+//	printf("\n Id elev: %d\n Nr materii: %d\n Nume elev: %s\n Nr de optionale alese sunt: %d\n Notele sunt: \n ", e.idElev, e.nrMaterii, e.numeElev,e.nrOptionale);
+//	for (int i = 0; i < e.nrMaterii; i++)
+//	{
+//		printf("\n%.2f\n", e.note[i]);
+//	}
+//
+//}
+//
+//void modificare_nrOptionale(struct Elev* e, int nrNou)
+//{
+//	if (nrNou > 0 && nrNou<5)
+//	{
+//		e->nrOptionale = nrNou;
+//	}
+//}
+//
+//void dezalocare(struct Elev* e)
+//{
+//	free(e->note);
+//	e->note = NULL;
+//
+//	free(e->numeElev);
+//	e->numeElev = NULL;
+//}
+//
+//
+//int main()
+//{
+//	float note[] = { 10,9.7 };
+//	struct Elev e1 = initializare(1, 2, note, "ionel",3);
+//	afisare(e1);
+//	modificare_nrOptionale(&e1,4);
+//	afisare(e1);
+//
+//	dezalocare(&e1);
+//
+//}
